e44022b9183c3fa1d98e31fdf783532a958d8320
Mateus <mateusandreferreira@gmail.com>
2020-09-16 19:59:45 -0400

    first commit

---
+++ dist/precache-manifest.a6b327fc3ddfd9a709b95a060c995083.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc977f43c0c5374898b",
+    "url": "/css/app.70bbf822.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "da3a1e4035ae8d6fddb8803aeee8e038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a03d2b00d583f6a60eb",
+    "url": "/js/about.c787ca3d.js"
+  },
+  {
+    "revision": "efc977f43c0c5374898b",
+    "url": "/js/app.47aed032.js"
+  },
+  {
+    "revision": "82117e3fc77aac913dcf",
+    "url": "/js/chunk-vendors.754f45b6.js"
+  },
+  {
+    "revision": "af2a578fba46c0abd3499796085dda6c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
