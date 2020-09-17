9e709b085a77f49567ceb8bcc9bcb8a53ae773c0
Mateus <mateusandreferreira@gmail.com>
2020-09-16 20:56:30 -0400

    Altera server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const serverStatic = require('server-static');
-const path = require('path');
+const express = require("express");
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use('/', serverStatic(path.join(__dirname, '/dist')));
-
-const port = process.env.PORT || 8080;
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
 app.listen(port);
 
-console.log('listen on port: ' + port);
+console.log("Server started...");
